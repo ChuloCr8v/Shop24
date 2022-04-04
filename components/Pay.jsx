00c03447a130b43c9cb5afc1787f49b811f2f796
chulocr8v <nkematu5@gmail.com>
2022-04-04 20:48:32 +0100

    updates

--- components/Pay.jsx
+++ components/Pay.jsx
@@ -1,37 +1,46 @@
 import axios from "axios";
 import StripeCheckout from "react-stripe-checkout";
-import { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import {
+  useState
+} from "react";
+import {
+  useEffect
+} from "react/cjs/react.development";
 
 const Pay = () => {
   const KEY =
-    "pk_test_51KkiDeAUFaW3s2nQRhJZ6vn0YcIoAfos8Q8ulVMRb2ihdtrGWoUaek2QEnbBbv5jcK2mOpTE36hj6sKyji0Zrkli00jPqIuVHH";
+  "pk_test_51KkiDeAUFaW3s2nQRhJZ6vn0YcIoAfos8Q8ulVMRb2ihdtrGWoUaek2QEnbBbv5jcK2mOpTE36hj6sKyji0Zrkli00jPqIuVHH";
 
-  const [stripeToken, setStripeToken] = useState(null);
+  const [stripeToken,
+    setStripeToken] = useState(null);
   useEffect(() => {
-    try {
-      const res = axios.post("http://localhost:5000/api/checkout/payment", {
-        amount: 2000,
-        tokenId: stripeToken.id,
-      });
-    } catch (error) {
-      console.log(error);
+    const makeRequest = async () => {
+      try {
+        const res = await axios.post("http://localhost:5000/api/checkout/payment", {
+          amount: 2000,
+          tokenId: stripeToken.id,
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
+
+    stripeToken && makeRequest()
   }, [stripeToken]);
 
   const onToken = (token) => {
-    console.log(token);
+    setStripeToken(token)
   };
   return (
     <div
-      style={{
+      style={ {
         height: "100vh",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
       }}
-    >
+      >
       <StripeCheckout
         description="your total is 1000 USD"
         amount={2000}
@@ -40,7 +49,7 @@ const Pay = () => {
         billingAddress
         ShippingAddress
         name="shop24seven"
-      >
+        >
         <button>Pay</button>
       </StripeCheckout>
     </div>
