d9245b39b3d149fd24759724cd49b56e79226ff2
Nkematu Bonaventure <nkematu5@gmail.com>
2022-04-04 18:09:29 +0200

    updates

--- components/Pay.jsx
+++ components/Pay.jsx
@@ -1,8 +1,50 @@
-import axios from 'axios'
+import axios from "axios";
+import StripeCheckout from "react-stripe-checkout";
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 
 const Pay = () => {
-  return(
-    )
-}
+  const KEY =
+    "pk_test_51KkiDeAUFaW3s2nQRhJZ6vn0YcIoAfos8Q8ulVMRb2ihdtrGWoUaek2QEnbBbv5jcK2mOpTE36hj6sKyji0Zrkli00jPqIuVHH";
 
-export default Pay
+  const [stripeToken, setStripeToken] = useState(null);
+  useEffect(() => {
+    try {
+      const res = axios.post("http://localhost:5000/api/checkout/payment", {
+        amount: 2000,
+        tokenId: stripeToken.id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [stripeToken]);
+
+  const onToken = (token) => {
+    console.log(token);
+  };
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <StripeCheckout
+        description="your total is 1000 USD"
+        amount={2000}
+        token={onToken}
+        stripeKey={KEY}
+        billingAddress
+        ShippingAddress
+        name="shop24seven"
+      >
+        <button>Pay</button>
+      </StripeCheckout>
+    </div>
+  );
+};
+
+export default Pay;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,39 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Hero from '../components/Hero'
-import FeaturedProducts from '../components/FeaturedProducts'
-import ShoeCat from '../components/ShoeCat'
-import BagCat from '../components/BagCat'
-import WatchCat from '../components/WatchCat'
-import ShadeCat from '../components/ShadeCat'
-import Cta from '../components/Cta'
-import Subscribe from '../components/Subscribe'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Hero from "../components/Hero";
+import FeaturedProducts from "../components/FeaturedProducts";
+import ShoeCat from "../components/ShoeCat";
+import BagCat from "../components/BagCat";
+import WatchCat from "../components/WatchCat";
+import ShadeCat from "../components/ShadeCat";
+import Cta from "../components/Cta";
+import Subscribe from "../components/Subscribe";
+import Pay from "../components/Pay";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Shop 24Seven</title>
-        <meta name="description" content="Your N0.1 online store for all fashion accessories. Shoes | Winter Jackets | Bags | Thermal Wears" />
+        <meta
+          name="description"
+          content="Your N0.1 online store for all fashion accessories. Shoes | Winter Jackets | Bags | Thermal Wears"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <Hero />
+        {/*<Hero />
         <FeaturedProducts />
         <ShoeCat />
         <BagCat />
         <WatchCat />
         <Cta />
         <ShadeCat />
-        <Subscribe />
+        <Subscribe />*/}
+        <Pay />
       </main>
     </div>
-  )
+  );
 }
