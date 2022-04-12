b4b779e671ca9b3cfefc7c7bcea6f08d0169f941
Nkematu Bonaventure <nkematu5@gmail.com>
2022-04-13 01:05:54 +0200

    u

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,52 +1,53 @@
-import styles from '../styles/FeaturedProduct.module.scss'
-import {FaShoppingCart, FaHeart, FaSearch} from 'react-icons/fa'
-import Link from 'next/link'
+import styles from "../styles/FeaturedProduct.module.scss";
+import { FaShoppingCart, FaHeart, FaSearch } from "react-icons/fa";
+import Link from "next/link";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
-      <div className={styles.product}>
-        <div className={styles.img_container}>
-          <img
-            src={product.product_main_image_url}
-            alt={product.name}
-            className={styles.product_img}
-          />
-        </div>
-        <div className={styles.product_details}>
-          <div className={styles.cart_container} >
-            <div className={styles.cart}>
-              <FaShoppingCart className={styles.icon} />
-              <p>Add to Cart</p>
-            </div>
-            <Link 
+    <div className={styles.product}>
+      <div className={styles.img_container}>
+        <img
+          src={product.product_main_image_url}
+          alt={product.name}
+          className={styles.product_img}
+        />
+      </div>
+      <div className={styles.product_details}>
+        <div className={styles.cart_container}>
+          <div className={styles.cart}>
+            <FaShoppingCart className={styles.icon} />
+            <p>Add to Cart</p>
+          </div>
+          <Link
             href={{
-                  pathname: "/[id]",
-                  query: {
-                    title: product.title,
-                    id: "product",
-                    price: product.price,
-                    desc: product.desc,
-                  },
-                }}
-            >
-              <a className={styles.visit}>
-                <FaSearch class={styles.icon} />
-              </a>
-            </Link>
-          </div>
-          <div className={styles.product_detail}>
-            <p className={styles.name}>{product.title}</p>
-          </div>
-          <div className={styles.price_container}>
-            <p className={styles.price}>
-              {product.price}
-            </p>
-            <div className={styles.fav}>
-              <FaHeart className={styles.icon} />
-              <p>Save</p>
-            </div>
+              pathname: "/[id]",
+              query: {
+                title: product.title,
+                id: "product",
+                price: product.price,
+                desc: product.desc,
+                color: product.color,
+                size: product.size,
+              },
+            }}
+          >
+            <a className={styles.visit}>
+              <FaSearch class={styles.icon} />
+            </a>
+          </Link>
+        </div>
+        <div className={styles.product_detail}>
+          <p className={styles.name}>{product.title}</p>
+        </div>
+        <div className={styles.price_container}>
+          <p className={styles.price}>{product.price}</p>
+          <div className={styles.fav}>
+            <FaHeart className={styles.icon} />
+            <p>Save</p>
           </div>
         </div>
       </div>
- )} 
+    </div>
+  );
+};
 export default Product;
--- pages/[product].js
+++ pages/[product].js
@@ -1,17 +1,27 @@
-import styles from '../styles/SingleProduct.module.scss' 
-import Image from 'next/image'
-import ProductImg from '../public/images/sneakers2.jpg'
-import {FaChevronLeft, FaChevronRight} from 'react-icons/fa'
-
+import styles from "../styles/SingleProduct.module.scss";
+import Image from "next/image";
+import ProductImg from "../public/images/sneakers2.jpg";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { useState } from "react";
 
 const SingleProduct = (props) => {
-  return(
+  const [orderQty, setOrderQty] = useState(1);
+
+  const handleClick = (type) => {
+    if (type === "increase") {
+      setOrderQty(orderQty + 1);
+    } else if (type === "decrease") {
+      orderQty > 1 && setOrderQty(orderQty - 1);
+    }
+  };
+
+  return (
     <div className={styles.single_product}>
       <div className={styles.product_image_container}>
         <Image src={ProductImg} height="400px" width="450px" alt="product" />
       </div>
       <div className={styles.product_details_container}>
-        <div className={styles.product_description_container} >
+        <div className={styles.product_description_container}>
           <h1 className={styles.product_title}>{props.title}</h1>
           <p>{props.desc} </p>
         </div>
@@ -20,47 +30,55 @@ const SingleProduct = (props) => {
           <div className={styles.color_filter}>
             <h3>Color</h3>
             <div className={styles.colors}>
-              <div styles={{background: 'white'}} className={styles.color}></div>
-              <div styles={{background: 'black'}} className={styles.color}></div>
-              <div styles={{background: 'red'}} className={styles.color}></div>
+              <select className={styles.filter}>
+                {props.color.map((c) => (
+                  <option styles={{ background: { c } }}>{c}</option>
+                ))}
+              </select>
             </div>
-          </div>
-          <div className={styles.size_filter}>
-            <h3>Size</h3>
-            <select className={styles.filter}>
-               <option>XS</option>
-               <option>S</option>
-               <option>M</option>
-               <option>L</option>
-               <option>XL</option>
-            </select>
-          </div>
+            <div className={styles.size_filter}>
+              <h3>Size</h3>
+              <select className={styles.filter}>
+                {props.size.map((s) => (
+                  <option>{s}</option>
+                ))}
+              </select>
+            </div>
           </div>
           <div className={styles.quantity_cart_container}>
-            <div className={styles.product_quantity} >
-              <FaChevronLeft className={styles.arrow} />
+            <div className={styles.product_quantity}>
+              <FaChevronLeft
+                className={styles.arrow}
+                onClick={() => handleClick("decrease")}
+              />
               <div className={styles.quantity}>
-                <p>1</p>
+                <p>{orderQty}</p>
               </div>
-              <FaChevronRight className={styles.arrow} />
+              <FaChevronRight
+                className={styles.arrow}
+                onClick={() => handleClick("increase")}
+              />
             </div>
             <button className={styles.add_to_cart_button}> ADD TO CART </button>
-         </div>
+          </div>
+        </div>
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default SingleProduct
+export default SingleProduct;
 
 export async function getServerSideProps(context) {
-//  console.log(context.query.id)
+  //  console.log(context.query.id)
   return {
     props: {
-     // id: context.query.id,
+      // id: context.query.id,
       price: context.query.price,
       title: context.query.title,
       desc: context.query.desc,
+      color: context.query.color,
+      size: context.query.size,
     },
   };
 }
